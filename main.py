edcef3f75825f71ed046a3907f00729049a03def
Olaghuhfad <158076142+Olaghuhfad@users.noreply.github.com>
2024-02-25 14:53:43 +0000

    Small edit
    put code into https://www.pythonchecker.com/ and made some changes

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import tkinter as tk
-from tkinter import *
-from tictactoe import TicTacToe
 from tictactoegui import TicTacToeGUI
 
 
 FONT = ("Arial", 24, "normal")
 
-root = Tk()
+root = tk.Tk()
 root.title("TicTacToe")
 root.minsize(width=600, height=800)
 
 def on_closing():
     GUI.save_streak()
+    root.destroy()
 
 
 GUI = TicTacToeGUI(root)
 
-# root.protocol("WM_DELETE_WINDOW", on_closing)
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,8 @@
-
 class TicTacToe:
 
+
     def __init__(self):
-
         self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
         self.playerPositions = []
         self.cpuPositions = []
 
@@ -12,12 +10,13 @@ class TicTacToe:
     def playPiece(self, pos, user):
         if user == "player":
             self.playerPositions.append(pos)
-            self.board[pos-1] = "X"
+            self.board[pos - 1] = "X"
         elif user == "cpu":
             self.cpuPositions.append(pos)
-            self.board[pos-1] = "O"
+            self.board[pos - 1] = "O"
         else:
             print("Error in play piece, not valid user.")
+
 
     def check_position_free(self, pos):
         if pos in self.playerPositions or pos in self.cpuPositions:
@@ -25,8 +24,10 @@ class TicTacToe:
         else:
             return True
 
+
     def check_win(self):
-        winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+        winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+                          [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
         for sublist in winning_combos:
             result = all(item in self.playerPositions for item in sublist)
             if result:
@@ -36,11 +37,14 @@ class TicTacToe:
                 return "cpu"
         return "false"
 
+
     def get_player_positions(self):
         return self.playerPositions
 
+
     def get_number_of_moves(self):
         return len(self.playerPositions) + len(self.cpuPositions)
+
 
     def check_tie(self):
         moves = self.get_number_of_moves()
@@ -48,6 +52,7 @@ class TicTacToe:
             return True
         else:
             return False
+
 
     def clear_positions(self):
         del self.playerPositions[:]
